136dc813ff8b3e36e06d3845a4a9fd3e38341521
Joel Harkes <jlharkes@gmail.com>
2016-05-23 14:26:12 +0200

    update

--- src/script.js
+++ src/script.js
@@ -3,68 +3,97 @@
  * @param  {HTMLElement} checkElement
  */
 var CheckboxBeauty = function (checkElement) {
-    /** @type {HTMLElement} */
-    if(checkElement.type !== 'checkbox')
+    'user strict';
+    if (checkElement.type !== 'checkbox')
         throw new Error('CheckboxBeauty can only beautify a checkbox')
     this.input = checkElement;
-    const input = checkElement;    
+    var input = checkElement;
+    var self = this;
+
+    this.options = {
+        class: "checkbox",
+        checkedClass: "checked",
+        afterImpersonate: function (inputEl) {
+            //inputEl.className += "hidden";
+            inputEl.style += ";display:none;";
+        }
+    }
+    // chache if its checked, needed?
+    this.checked = self.input.checked;
+
+    var impersonator = document.createElement("div");
+    this.impersonator = impersonator;
+
+    impersonator.className = "checkbox"
+    impersonator.tabIndex = 0; //Make him tab-able
+    if (self.checked)
+        impersonator.className += " checked";
+    self.input.parentNode.insertBefore(impersonator, input.nextSibling)
+    impersonator.innerText = "x";
+    this.options.afterImpersonate(self.input);
     
-    this.checked = input.checked;
     
-    var fakeBox = document.createElement("div");
-    var checked = input.checked;
-    fakeBox.className = "checkbox"
-    fakeBox.tabIndex = 0;
-    if(checked)
-        fakeBox.className += " checked";
-    input.parentNode.insertBefore(fakeBox, input.nextSibling)
-    fakeBox.innerText = "x";
-    //el.style = "display:none;";
-    var makeEvent = function (name) {
-        var ev = new Event(name);
-        ev.pluginMade = true;
-        return ev;
+    //Setup event listeners
+    this.impersonatorClick = function (ev) {
+        if (!ev.pluginMade) 
+            input.checked = !input.checked;
+        self.checked = input.checked;
+        if (self.checked)
+            impersonator.className += ' checked'
+        else
+            impersonator.className = 'checkbox'
+        if (!ev.pluginMade) {
+            input.dispatchEvent(self.makeEvent("click"));
+            input.dispatchEvent(self.makeEvent("change"));
+        }
+    }
+
+    this.impersonatorKeydown =  function (ev) {
+        //32 = space bar, normaly spacebar on checkbox fires mouse click event
+        if (ev.keyCode == 32) {
+            impersonator.dispatchEvent(new Event("click")); //Make it look like an original event
+        }
+    }
+    this.inputClick =  function (ev) {
+        //console.log("click", ev);
+        if (!ev.pluginMade) {
+            impersonator.dispatchEvent(self.makeEvent("click"))
+            //fakeBox.dispatchEvent(makeEvent("change"))
+        }
+    }
+    this.inputChange =  function (ev) {
+        //console.log("change", input.checked);
+    }
+
+    var inputInput = function (ev) {
+        console.log("input", input.checked);
     }
     
-    fakeBox.addEventListener("click",function (ev) {
-        if(!ev.pluginMade)
-            input.checked = !input.checked;
-            checked = input.checked;
-        if(checked)
-            fakeBox.className += ' checked'
-        else 
-            fakeBox.className = 'checkbox'
-        
-        if(!ev.pluginMade){
-            input.dispatchEvent(makeEvent("click"));
-            input.dispatchEvent(makeEvent("change"));
-        }
-        
-    });
     
-    fakeBox.addEventListener("keydown",function (ev) {
-        //32 = space bar, normaly spacebar on checkbox fires mouse click event
-       if (ev.keyCode == 32) {
-           fakeBox.dispatchEvent(new Event("click"));
-       } 
-    });
-    
-    input.addEventListener("click", function (ev) {
-        console.log("click",ev);
-        if(!ev.pluginMade){
-            fakeBox.dispatchEvent(makeEvent("click"))
-            //fakeBox.dispatchEvent(makeEvent("change"))
-            
-        }
-    });
-    
-    input.addEventListener("change", function (ev) {
-        console.log("change",input.checked);
-    });
-    
-    input.addEventListener("input", function (ev) {
-        console.log("input",input.checked);
-    });
-    
-    
+    this.addListeners();
+}
+
+CheckboxBeauty.prototype.makeEvent = function (name) {
+    var ev = new Event(name);
+    ev.pluginMade = true;
+    return ev;
+};
+
+CheckboxBeauty.prototype.addListeners = function () {
+    this.impersonator.addEventListener("click", this.impersonatorClick);
+    this.impersonator.addEventListener("keydown", this.impersonatorKeydown);
+    this.input.addEventListener("click",this.inputClick);
+    this.input.addEventListener("change",this.inputChange);
+    this.input.addEventListener("input", this.inputInput);
+};
+CheckboxBeauty.prototype.removeListeners = function () {
+    this.impersonator.removeEventListener("click", this.impersonatorClick);
+    this.impersonator.removeEventListener("keydown", this.impersonatorKeydown);
+    this.input.removeEventListener("click",this.inputClick);
+    this.input.removeEventListener("change",this.inputChange);
+    this.input.removeEventListener("input",this.inputInput);
+};
+CheckboxBeauty.prototype.destroy = function () {
+    this.removeListeners();
+    this.impersonator.remove();    
 }
--- test/test-checkbox.js
+++ test/test-checkbox.js
@@ -1,12 +1,134 @@
 /// <reference path="../src/script.js" />
 /// <reference path="../bower_components/jasmine-core/lib/jasmine-core/jasmine.js" />
 
-describe('the checkbox01', function() {
-    var checkbox01 = document.getElementById('test01');
-  it('should be upgradeable', function() {
-      var beauty = new CheckboxBeauty(checkbox01);
-    expect(beauty).not.toBe(null); //toEqual("DIV");
-    expect(beauty.input).toBe(checkbox01);
-    
+describe('a default checkbox in the page', function () {
+  /** @type {HTMLInputElement} */
+  var checkbox;
+
+  beforeEach(function () {
+    checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    document.body.appendChild(checkbox);
+  });
+  afterEach(function () {
+    checkbox.remove();
+  });
+
+  describe('and beautified', function () {
+    /** @type {CheckboxBeauty} beauty */
+    var beauty;
+
+    beforeEach(function () {
+      beauty = new CheckboxBeauty(checkbox);
+    });
+
+    afterEach(function () {
+      beauty.destroy();
+      beauty = null;
+    });
+
+    it('should be upgradeable', function () {
+      expect(beauty).not.toBe(null); //toEqual("DIV");
+      expect(beauty.input).toBe(checkbox);
+    });
+
+    it('should be unchecked', function () {
+      expect(beauty.checked).not.toBe(true);
+      expect(checkbox.checked).not.toBe(true);
+    });
+
+
+    describe('and (artificialy) clicked on input checkbox', function () {
+      var clickCount = 0;
+      var changeCount = 0;
+      /** @type {MouseEvent} */
+      var lastClick = null;
+      var clickListener = function (event) {
+        clickCount++;
+        lastClick = event;
+      }
+      var changeListener = function (event) {
+        changeCount++;
+      }
+      beforeEach(function () {
+        clickCount = 0;
+        changeCount = 0;
+        lastClick = null;
+        checkbox.parentElement.addEventListener("click", clickListener);
+        checkbox.parentElement.addEventListener("change", changeListener);
+
+        var evt = new MouseEvent('click', { bubbles : true});
+        checkbox.dispatchEvent(evt);
+      });
+
+      afterEach(function () {
+        checkbox.parentElement.removeEventListener("click", clickListener);
+        checkbox.parentElement.removeEventListener("change", changeListener);
+      });
+      it('should input be checked', function () {
+        expect(checkbox.checked).toBe(true);
+      });
+      it('should beautifier be checked', function () {
+        expect(beauty.checked).toBe(true);
+      });
+
+      it('click event should only buble once', function () {
+
+        expect(clickCount).toBe(1);
+      });
+
+
+      it('click event target should be the input checkbox', function () {
+        expect(lastClick.target).toBe(checkbox);
+      });
+
+      it('change event should only buble once', function () {
+        expect(changeCount).toBe(1);
+      });
+
+    });
+
+    describe('and then destroyed', function () {
+      beforeEach(function () {
+        beauty.destroy();
+      });
+      it('should not be in the dom anymore', function () {
+        expect(document.contains(beauty.impersonator)).not.toBe(true);
+      })
+    });
+  });
+
+  describe('clicked on (make it checked)', function () {
+    beforeEach(function () {
+      var evt = new MouseEvent('click');
+      checkbox.dispatchEvent(evt);
+    });
+
+    afterEach(function () {
+      //click again to uncheck
+      var evt = new MouseEvent('click');
+      checkbox.dispatchEvent(evt);
+    });
+
+    describe('and beautified', function () {
+      /** @type {CheckboxBeauty} */
+      var beauty;
+
+      beforeEach(function () {
+        beauty = new CheckboxBeauty(checkbox);
+      });
+
+      afterEach(function () {
+        beauty.destroy();
+        beauty = null;
+      });
+
+      it('should be checked', function () {
+        expect(beauty.checked).toBe(true);
+        expect(checkbox.checked).toBe(true);
+      });
+
+    });
+
   });
 });
