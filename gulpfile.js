53b1f841997fbc8a5a06fbd893776690be2834c8
Marjorie Burkhalter <Marjorie Burkhalter>
2016-10-20 22:22:04 +0200

    Cleanup apiffeny. css is the way to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,11 @@
 var gulp = require('gulp');
-//var rename = require("gulp-rename");
-var uglify = require('gulp-minify');
-
+var less = require('gulp-less');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
 gulp.task('default', function () {
     // place code for your default task here
-    return gulp.src('src/*.js')
-        .pipe(uglify(
-            {
-                ext: {
-                    min: ".min.js"
-                },
-                noSource : true
-            }
-        ))
-        //.pipe(rename({suffix : '.min'}))
+    return gulp.src('src/*.less')
         .pipe(gulp.dest('dist'));
 });
 
